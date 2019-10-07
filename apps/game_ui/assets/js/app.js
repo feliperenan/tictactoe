d516c1537fd96d39a95ba2535a47a2193e80fdca
Felipe Renan <feelipe.renan@gmail.com>
2019-10-07 22:58:50 +0300

    Pass Socket as argument to LiveSocket

--- apps/game_ui/assets/js/app.js
+++ apps/game_ui/assets/js/app.js
@@ -10,8 +10,9 @@ import css from "../css/app.css"
 // Import dependencies
 //
 import "phoenix_html"
+import { Socket } from "phoenix"
 
 import LiveSocket from "phoenix_live_view"
 
-let liveSocket = new LiveSocket("/live")
+let liveSocket = new LiveSocket("/live", Socket)
 liveSocket.connect()
